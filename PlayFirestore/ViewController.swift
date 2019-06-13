b632f096db9064b1b147a7ca94d4689830f2e3dc
beakchild <crt2112@gmail.com>
2019-06-13 22:45:39 +0900

    first commit

---
+++ PlayFirestore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayFirestore
+//
+//  Created by beakhand on 2019/06/13.
+//  Copyright © 2019 beakhand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

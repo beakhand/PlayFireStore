a04a59c37e43ed195cb33df95400195e61ce3fdf
beakchild <crt2112@gmail.com>
2019-06-27 10:00:38 +0900

    - setup firebase - firestore add document test

--- PlayFirestore/ViewControllers/ViewController.swift
+++ PlayFirestore/ViewControllers/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import Firebase
+
+
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func onButtonTap(_ sender: Any) {
+        addDocument()
+    }
+    
+    func addDocument() {
+        let db = Firestore.firestore()
+        var ref: DocumentReference?
+        ref = db.collection("users").addDocument(data: [
+            "first": "Aba",
+            "last": "Lovelace",
+            "born": 1815
+        ]) { err in
+            if let err = err {
+                print("Error add document \(err)")
+            } else {
+                let id = ref.map { $0.documentID } ?? "id none"
+                print("Success add document - ID -> \(id)")
+            }
+        }
+    }
 }
 

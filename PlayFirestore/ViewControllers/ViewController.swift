afbb72c9c19ddb67edbbce9369f70000128f8948
beakchild <crt2112@gmail.com>
2019-10-08 22:07:20 +0900

    change chat ui

--- PlayFirestore/ViewControllers/ViewController.swift
+++ PlayFirestore/ViewControllers/ViewController.swift
@@ -8,60 +8,14 @@
 
 import UIKit
 import Firebase
-import ExpandableLabel
 import RxCocoa
 import RxSwift
 
-class ViewController: UIViewController {
-    @IBOutlet weak var button: UIButton!
-    @IBOutlet weak var label: ExpandableLabel!
-    @IBOutlet weak var exLabel: ExpandableLabel!
-    @IBOutlet weak var textField: UITextField!
-    
+class ViewController: UIViewController {    
     private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.numberOfLines = 1
-        label.collapsed = true
-        label.collapsedAttributedLink = NSAttributedString(string: "read more")
-        
-        exLabel.text = "あいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおかきくけこかきくけこかきくけこ"
-        exLabel.numberOfLines = 2
-        exLabel.expandedAttributedLink = NSAttributedString(string: "Read Less")
-        exLabel.collapsedAttributedLink = NSAttributedString(string: "Read More")
-
-        DispatchQueue.main.async { [weak self] in
-
-            //self?.exLabel.collapsedAttributedLink = NSAttributedString(string: "Read More")
-            
-        }
-        
-        //let textField: UITextField
-        //textField.rx.
-        textField.rx.text.asDriver().drive(Binder(self) { _, text in
-            print(text)
-        }).disposed(by: self.disposeBag)
-
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        //label.collapsedAttributedLink = NSAttributedString(string: "Read More")
-        //label.numberOfLines = 2
-        
-        DispatchQueue.main.asyncAfter(deadline: .now()+2, execute: {
-//            self.label.addTrailing(with: "...", moreText: "read more", moreTextFont: self.label.font, moreTextColor: self.label.textColor)
-        })
-
-    }
-    
-    @IBAction func onAddDocumentButtonTap(_ sender: Any) {
-        addDocument()
-    }
-    
-    @IBAction func onGetDocumentButtonTap(_ sender: Any) {
-        getDocument()
     }
     
     
@@ -69,42 +23,9 @@ class ViewController: UIViewController {
         return Firestore.firestore()
     }
     
-    private func addDocument() {
-        var ref: DocumentReference?
-        ref = db.collection("users").addDocument(data: [
-            "first": "Aba",
-            "last": "Lovelace",
-            "born": 1815
-        ]) { err in
-            if let err = err {
-                print("Error add document \(err)")
-            } else {
-                let id = ref.map { $0.documentID } ?? "id none"
-                print("Success add document - ID -> \(id)")
-            }
-        }
-    }
-    
-    private func getDocument() {
-        db.collection("users").getDocuments() { querySnapshot, err in
-            if let err = err {
-                print("Error ge document \(err)")
-            } else {
-                guard let query = querySnapshot else { return }
-                query.documents.forEach { doc in
-                    print("get document id -> \(doc.documentID) : data -> \(doc.data())")
-                }
-            }
-        }
-    }
-    
-    private func listenForUsers() {
-        db.collection("users")
-            .whereField("born", isLessThan: 1900)
-        
-            .addSnapshotListener {querySnapshot, error in
-                
-        }
-        
-    }
 }
+
+//        textField.rx.text.asDriver().drive(Binder(self) { _, text in
+//            print(text)
+//        }).disposed(by: self.disposeBag)
+
--- Podfile
+++ Podfile
@@ -4,6 +4,7 @@
 target 'PlayFirestore' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   pod 'Firebase/Core'
+  pod 'Firebase/Auth'
   pod 'Firebase/Firestore'
   pod 'ExpandableLabel'
 
